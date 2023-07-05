19b2f8442ee39b0f3671212c3be4d90162c86713
zhanghongbin12 <hongbinzhang@zshield.net>
2023-07-05 11:10:52 +0800

    feat: 创建pwa项目

---
+++ dist/precache-manifest.d8d764d74e3eb1d638ecf70bd5545bd7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26617ee4b9a08559bd1",
+    "url": "css/app.c0757b52.css"
+  },
+  {
+    "revision": "a26617ee4b9a08559bd1",
+    "url": "css/app.c0757b52.css.map"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a26617ee4b9a08559bd1",
+    "url": "js/app.7b72f091.js"
+  },
+  {
+    "revision": "a26617ee4b9a08559bd1",
+    "url": "js/app.7b72f091.js.map"
+  },
+  {
+    "revision": "298f09e7adb04761c933",
+    "url": "js/chunk-vendors.0b43d30e.js"
+  },
+  {
+    "revision": "298f09e7adb04761c933",
+    "url": "js/chunk-vendors.0b43d30e.js.map"
+  },
+  {
+    "revision": "77b5cb1d99cdf4feee509df7b0935d53",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    
+    publicPath: './',
+    pwa: { 
+        name: 'xxx',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            exclude: [/\.html$/],
+            //html不进行service Worker缓存// 自定义 Service Worker 文件的位置
+            swSrc: 'src/registerServiceWorker.js'
+        }
+    }
+
+}
